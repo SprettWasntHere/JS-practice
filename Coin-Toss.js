9e37a8d6fe9b001e53ca1483dc028bf51113190f
SprettWasntHere <sprett50@gmail.com>
2025-11-14 12:23:25 +0100

    Refactor Coin Toss game loop for clarity and efficiency

--- Coin-Toss.js
+++ Coin-Toss.js
@@ -1,25 +1,29 @@
 const prompt = require("prompt-sync")();
 const coinSides = ["HEADS", "TAILS"];
 
-function GameLoop() {
+while true {
     console.clear();
     
     let randomChoice = coinSides[Math.floor(Math.random() * 2)];
-
+    
     let answer = prompt("Heads or Tails? ");
 
+    if (answer == '') {
+        break;
+    };
+    
     if (!coinSides.includes(answer.toUpperCase())) {
         prompt("Sorry, your input is invalid. :( ")
-        GameLoop()
+        continue;
     };
-
+    
     if (answer.toUpperCase() == randomChoice) {
-        prompt("You win! You guessed " + answer + " and it was CORRECT! :D Press ENTER to play again!")
+        prompt("You win! You guessed ${answer} and it was CORRECT! :D Press ENTER to play again!")
     } else {
-        prompt("You lost. You guessed " + answer + " but it was " + randomChoice.toLowerCase() + ". :( Press ENTER to play again!")
+        prompt("You lost. You guessed ${answer} but it was " + randomChoice.toLowerCase() + ". :( Press ENTER to play again!")
     };
-
-    GameLoop();
+    continue;
 };
+
 
 GameLoop();
